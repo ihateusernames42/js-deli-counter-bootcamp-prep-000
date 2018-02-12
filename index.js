274f612ee4be51138f76fad72654da897ec97196
Dylan L Jones <darkraikiri@gmail.com>
2018-02-12 18:51:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ function takeANumber(l,n){
   var x = l.length + 1 
   return `Welcome, ${n} You are number ${x} in line.`
 }
-function nowServing(){
-  if (katzDeli.length === 0){
-    return "There "
+function nowServing(d){
+  if (d.length === 0){
+    return "There is nobody waiting to be served!"
+  }
+  else { 
+    var n = d[0]
+    unshift.d[0]
+    return n
   }
 }
+function currentLine(d)
